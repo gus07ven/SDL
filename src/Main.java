201e5567ffd61bd96c67d6aec25d8f702b650acb
gus07ven <gustavoarismendi@hotmail.com>
2018-01-30 15:11:12 -0600

    Add main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Employee emp = new Employee();
+        emp.setName("Mickey");
+        System.out.println(emp.getName());
+
+    }
+}
