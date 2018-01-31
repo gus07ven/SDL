d8bd25e612cb9072c8e814248134fc5729fb6bef
gus07ven <gustavoarismendi@hotmail.com>
2018-01-31 17:19:57 -0600

    Modify Shape to follow open/closed

--- src/Shape.java
+++ src/Shape.java
@@ -1,23 +1,54 @@
 // A class that doesn't follow the Open/Closed principle
-public class Rectangle(){
-    public double length;
-    public double width;
-}
-
-public class Circle(){
-    public double radius;
-}
+//public class Rectangle(){
+//    public double length;
+//    public double width;
+//}
+//
+//public class Circle(){
+//    public double radius;
+//}
 
 ////As new shapes are added, the class keeps changing. Design is not closed for modifications.
 //// This design is not extensible because we keep adding functionality little by little to AreaCalculator.
-public class AreaCalculator(){
+//public class AreaCalculator(){
+//
+//    public double calculateRectangleArea(Rectangle rectangle){
+//        return rectangle.length * rectangle.width;
+//        }
+//
+//    public double calculateCircleArea(Circle circle){
+//        return (22/7) * circle.radius * circle.radius;
+//        }
+//}
 
-    public double calculateRectangleArea(Rectangle rectangle){
-        return rectangle.length * rectangle.width;
-        }
+// Design changes to make Shape extensible.
+// Base interface Shape
+public interface Shape(){
 
-    public double calculateCircleArea(Circle circle){
-        return (22/7) * circle.radius * circle.radius;
+    // Abstract method calculate area
+    public double calculcateArea(){
         }
 }
 
+public class Rectangle implements Shape {
+    public double length;
+    public double width;
+
+    public double calculateArea(){
+        return length * width;
+    }
+}
+
+public class Circle implements Shape {
+    public double radius;
+
+    public double calculateArea(){
+        return (22/7) * radius * radius;
+    }
+}
+
+public class AreaCalculator{
+    public double calculateShapeArea(Shape shape){
+        return shape.calculateArea();
+    }
+}
